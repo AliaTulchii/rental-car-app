347a39b3d2d8cc21e56f1f6a64413c5d856c2d9f
Halina Tulchii <halendeihalyna@gmail.com>
2023-09-20 21:18:16 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter >
+    <BrowserRouter basename='rental-car-app'>
       <App />
         </BrowserRouter>
       </PersistGate>
